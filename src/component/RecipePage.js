8df7665901f478243ff90f4a1a19a23ad5401464
itsakanksha10 <itsakanksha10@gmail.com>
2022-01-26 05:34:19 +0530

    recipe page changed

--- src/component/RecipePage.js
+++ src/component/RecipePage.js
@@ -27,26 +27,59 @@ const RecipePage =() =>{
     if(Recipe){
         return (
             <>
-                <div class="full-page">
-                    <div class="food-container">
-                        <div class="food-info">
-                            <div class="info-head">
-                                <div class="food-name">
-                                {/* <span class="food-name-value">Mixed Berry Melody</span> */}
+                {/* <div className="full-page">
+                    <div className="food-container">
+                        <div className="food-info">
+                            <div className="info-head">
+                                <div className="food-name">
+                                <span className="food-name-value">Mixed Berry Melody</span>
                             </div>
                         </div>
-                        <div class = "instructions">
-                            <p class="instructions-head">{Recipe.strMeal}</p>
-                            <p class ="instructions-body">{Recipe.strInstructions}</p>
+                        <div className = "instructions">
+                            <p className="instructions-head">{Recipe.strMeal}</p>
+                            <p className ="instructions-body">{Recipe.strInstructions}</p>
+                             <a href={Recipe.strYoutube} target="_blank" classNameName="btn btn-primary">Check Youtube Video</a>
+
                         </div>
                     </div>
                     </div>
-                    <div class="food-img"><img className='foodimg' src={Recipe.strMealThumb} alt="" /></div>
-                </div>
+                    <div className="food-img"><img classNameName='foodimg' src={Recipe.strMealThumb} alt="" /></div>
+                </div>  */}
+                <div className= 'container fulpage'>
+                    <h1>{Recipe.strMeal}</h1>  
+                        <div className="banner"  style={{ backgroundImage: `url(${Recipe.strMealThumb})` }}>
+                            <div className="tintlayer">
+                                <p>hi</p>
+                           </div>       
+                        </div>
+                        <div className="bodycontent">
+                            <div className="heading">
+                                <div className="ins"><h1>Instructions</h1></div>
+                                <div className="method"><h1>Method</h1></div>
+                            </div>
+                             <hr className="new1"/>
+                            <div className="bodytxt">
+                                <div className="ingridients">
+                                    <ul>
+                                        <li>100 ml milk</li>
+                                        <li>3 eggs</li>
+                                        <li>50 g butter</li>
+                                        <li>50 g butter</li>
+                                        <li>a pinch of salt</li>
+                                    </ul>
+                                </div>
+                                <div className="method">
+                                    <p>{Recipe.strInstructions}</p>
+                                <hr className="new1"/>
+                                </div>
+                            </div>
+                        </div>
+                </div>   
             </>
         );
     }else{
-        return <div></div>
+        return 
+        <div></div>
     }
     
 }
